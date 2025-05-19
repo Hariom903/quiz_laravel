64dcf71f35ebeebb04d4844112b896f2fea8701f
hariom903 <hariomdangi266@gmail.com>
2025-05-19 03:06:57 -0700

    admin login

---
+++ resources/views/admin-login.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container justify-content-center align-content-center row min-vw-100 min-vh-100">
+        <h2 class="text-center text-success" > Login Page here....! </h2>
+        <form action="" class="" method="post">
+        <div class="mb-3 offset-sm-3 col-12 col-sm-6">
+            <label for="" class="form-label">Email</label>
+            <input
+                type="email"
+                class="form-control"
+                name=""
+                id=""
+                aria-describedby="emailHelpId"
+                placeholder="abc@mail.com" />
+        </div>
+        <div class="mb-3 offset-sm-3  col-12 col-sm-6">
+            <label for="" class="form-label">Password</label>
+            <input
+                type="password"
+                class="form-control"
+                name=""
+                id=""
+                placeholder=""
+            />
+        </div>
+           <div class="mb-3 offset-sm-3  col-12 col-sm-6">
+            <button class="btn btn-info form-control" type="submit"> Login  </button>
+           </div>
+
+
+    
+    </div>
+</form>
+
+</body>
+
+</html>
